e82e5a013aad33bdc35338110c763d451fe1bfa5
Leon <92785719+digifactory2@users.noreply.github.com>
2023-03-08 09:25:13 +0700

    Update Account Menu List

--- lib/accountmenu/account_menu.dart
+++ lib/accountmenu/account_menu.dart
@@ -105,7 +105,7 @@ class AccountMenu extends StatelessWidget {
                                         paddinghorizontal(size: 20),
                                         Container(
                                           child: Image.asset(
-                                            'assets/images/kuponNew.png',
+                                            'lib/icons/kuponNew.png',
                                             height: 50,
                                             width: 50,
                                           ),
@@ -157,7 +157,7 @@ class AccountMenu extends StatelessWidget {
                                         paddinghorizontal(size: 20),
                                         Container(
                                           child: Image.asset(
-                                            'assets/images/evouchernew.png',
+                                            'lib/icons/evouchernew.png',
                                             height: 50,
                                             width: 50,
                                           ),
@@ -207,10 +207,11 @@ class AccountMenu extends StatelessWidget {
                   child: Column(
                     children: [
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'My Score',
                           menuTxt:
                               'Checkout your score performance and see promotional offers',
-                          menuIcon: 'assets/images/score-nilaiQ-beta.png'),
+                          menuIcon: 'lib/icons/score-nilaiQ-beta.png'),
                     ],
                   ),
                 )),
@@ -231,27 +232,41 @@ class AccountMenu extends StatelessWidget {
                 Center(
                   child: Column(
                     children: [
-                      AccountSettingsList(
-                          menuTitle: 'Edit my personal data',
-                          menuTxt: 'Keep your personal data updated',
-                          menuIcon: 'assets/images/editProfile.png'),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const AccountEditProfile(),
+                              ),
+                            );
+                          },
+                          child: AccountSettingsList(
+                              menu: 'account',
+                              menuTitle: 'Edit my personal data',
+                              menuTxt: 'Keep your personal data updated',
+                              menuIcon: 'lib/icons/editProfile.png')),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Change EasyPIN',
                           menuTxt:
                               'Change your current EasyPIN for login and other verification purpose',
-                          menuIcon: 'assets/images/easypin.png'),
+                          menuIcon: 'lib/icons/easypin.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Change Password',
                           menuTxt:
                               'Change your current password for login and other verification purpose',
-                          menuIcon: 'assets/images/password.png'),
+                          menuIcon: 'lib/icons/password.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Internet Banking',
                           menuTxt: 'Manage your internet Banking access',
-                          menuIcon: 'assets/images/logoIB.png'),
+                          menuIcon: 'lib/icons/logoIB.png'),
                       greyLine(),
                     ],
                   ),
@@ -274,26 +289,30 @@ class AccountMenu extends StatelessWidget {
                   child: Column(
                     children: [
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Login preferences',
                           menuTxt: 'Manage your login options',
-                          menuIcon: 'assets/images/loginPre.png'),
+                          menuIcon: 'lib/icons/loginPre.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Language',
                           menuTxt: 'Choose your prefered languages',
-                          menuIcon: 'assets/images/language.png'),
+                          menuIcon: 'lib/icons/language.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Change device',
                           menuTxt:
                               'Move your authenticated device into a fresh new one',
-                          menuIcon: 'assets/images/change.png'),
+                          menuIcon: 'lib/icons/change.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Clear SimobiPlus data',
                           menuTxt:
                               'Clearing app data resets the application to scratch',
-                          menuIcon: 'assets/images/cleardata.png'),
+                          menuIcon: 'lib/icons/cleardata.png'),
                       greyLine(),
                     ],
                   ),
@@ -316,40 +335,169 @@ class AccountMenu extends StatelessWidget {
                   child: Column(
                     children: [
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Manage BI Ffast',
                           menuTxt: 'Manage transfer using BI Fast',
-                          menuIcon: 'assets/images/biFasticon.png'),
+                          menuIcon: 'lib/icons/biFasticon.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Scheduled transfer',
                           menuTxt: 'Keep all your scheduled transfer',
-                          menuIcon: 'assets/images/schedule.png'),
+                          menuIcon: 'lib/icons/schedule.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Favorite Transaction',
                           menuTxt: 'List of your favorite transaction',
-                          menuIcon: 'assets/images/favtrx.png'),
+                          menuIcon: 'lib/icons/favtrx.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Scheduled Bill Payment',
                           menuTxt:
                               'Create and track your scheduled bill payment',
-                          menuIcon: 'assets/images/autodebet.png'),
+                          menuIcon: 'lib/icons/autodebet.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Request for Payment from 3rd Party',
                           menuTxt: 'Check and pay your payment request now!',
-                          menuIcon: 'assets/images/pushinvoice.png'),
+                          menuIcon: 'lib/icons/pushinvoice.png'),
                       greyLine(),
                       AccountSettingsList(
+                          menu: 'account',
                           menuTitle: 'Set Your Own Transfer Limit',
                           menuTxt: 'You can set your transfer limit easily',
-                          menuIcon: 'assets/images/limit.png'),
+                          menuIcon: 'lib/icons/limit.png'),
                       greyLine(),
                     ],
                   ),
                 ),
                 paddingvertical(size: 40),
+                Row(
+                  children: [
+                    paddinghorizontal(size: 20),
+                    Text(
+                      'Product Settings',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          fontFamily: 'Roboto'),
+                    ),
+                  ],
+                ),
+                paddingvertical(size: 20),
+                Center(
+                  child: Column(
+                    children: [
+                      AccountSettingsList(
+                          menu: 'product',
+                          menuTitle: 'Manage Account and ATM Card'),
+                      greyLine(),
+                      AccountSettingsList(
+                          menu: 'product',
+                          menuTitle: 'Apply and Manage Merchant'),
+                    ],
+                  ),
+                ),
+                paddingvertical(size: 40),
+                Row(
+                  children: [
+                    paddinghorizontal(size: 20),
+                    Text(
+                      'Help & support',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          fontFamily: 'Roboto'),
+                    ),
+                  ],
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      AccountSettingsList(
+                          menu: 'help',
+                          menuTitle: 'Call support 1500153',
+                          menuIcon: 'phone'),
+                      greyLine(),
+                      AccountSettingsList(
+                          menu: 'help',
+                          menuTitle: 'Call support (021) 50188888',
+                          menuIcon: 'phone'),
+                      greyLine(),
+                      AccountSettingsList(
+                          menu: 'product',
+                          menuTitle: 'Frequently asked questions'),
+                      greyLine(),
+                      AccountSettingsList(
+                          menu: 'product', menuTitle: 'ATM & branch locator'),
+                      greyLine(),
+                      AccountSettingsList(
+                          menu: 'help',
+                          menuTitle: 'Chat with Prissa',
+                          menuIcon: 'phone'),
+                    ],
+                  ),
+                ),
+                paddingvertical(size: 40),
+                Row(
+                  children: [
+                    paddinghorizontal(size: 20),
+                    Text(
+                      'Legal',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          fontFamily: 'Roboto'),
+                    ),
+                  ],
+                ),
+                paddingvertical(size: 20),
+                Center(
+                  child: Column(
+                    children: [
+                      AccountSettingsList(
+                          menu: 'product', menuTitle: 'Terms and Conditions'),
+                      greyLine(),
+                      AccountSettingsList(
+                          menu: 'product', menuTitle: 'Privacy Policy'),
+                    ],
+                  ),
+                ),
+                paddingvertical(size: 20),
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'SimobiPlus Version 8.26.1',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+                paddingvertical(size: 40),
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xffFA3F70)),
+                    child: SizedBox(
+                        //width: 100,
+                        width: MediaQuery.of(context).size.width -
+                            40, //this is the total width of your screen
+                        height: 47,
+                        child: Center(
+                          child: Text(
+                            'LOG OUT',
+                            style: TextStyle(
+                                color: Colors.white, fontFamily: 'Roboto'),
+                          ),
+                        )),
+                  ),
+                ),
+                paddingvertical(size: 65),
               ],
             )
           ],
--- lib/accountmenu/account_settings_list.dart
+++ lib/accountmenu/account_settings_list.dart
@@ -3,78 +3,219 @@ import 'package:simobiplus/styling/paddinghorizontal.dart';
 import 'package:simobiplus/styling/paddingvertical.dart';
 
 class AccountSettingsList extends StatelessWidget {
+  final String menu;
   final String menuTitle;
   final String menuTxt;
   final String menuIcon;
 
   const AccountSettingsList({
     Key? key,
+    required this.menu,
     required this.menuTitle,
-    required this.menuTxt,
-    required this.menuIcon,
+    this.menuTxt = '',
+    this.menuIcon = '',
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: MediaQuery.of(context).size.width - 40,
-        child: Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                paddingvertical(size: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    paddinghorizontal(size: 20),
-                    Row(
-                      children: [
-                        Container(
-                          child: Image.asset(
-                            menuIcon,
-                            height: 35,
-                            width: 35,
-                          ),
-                        ),
-                        paddinghorizontal(size: 20),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              //width: 100,
-                              width: MediaQuery.of(context).size.width -
-                                  180, //this is the total width of your screen
-                              child: Text(
-                                menuTitle,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    fontFamily: 'Roboto'),
-                              ),
+    if (this.menu == 'account') {
+      return SizedBox(
+          width: MediaQuery.of(context).size.width - 40,
+          child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.white),
+              child: Column(
+                children: [
+                  paddingvertical(size: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      paddinghorizontal(size: 20),
+                      Row(
+                        children: [
+                          Container(
+                            child: Image.asset(
+                              menuIcon,
+                              height: 35,
+                              width: 35,
                             ),
-                            SizedBox(
-                              //width: 100,
-                              width: MediaQuery.of(context).size.width -
-                                  180, //this is the total width of your screen
-                              child: Text(
-                                menuTxt,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w300,
-                                    fontFamily: 'Roboto'),
-                                // overflow: TextOverflow.ellipsis,
-                                // softWrap: false,
+                          ),
+                          paddinghorizontal(size: 20),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                //width: 100,
+                                width: MediaQuery.of(context).size.width -
+                                    180, //this is the total width of your screen
+                                child: Text(
+                                  menuTitle,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      fontFamily: 'Roboto'),
+                                ),
                               ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    Icon(Icons.arrow_forward_ios, color: Color(0xffFA3F70)),
-                    paddinghorizontal(size: 20),
-                  ],
-                ),
-                paddingvertical(size: 25),
-              ],
-            )));
+                              SizedBox(
+                                //width: 100,
+                                width: MediaQuery.of(context).size.width -
+                                    180, //this is the total width of your screen
+                                child: Text(
+                                  menuTxt,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                      fontFamily: 'Roboto'),
+                                  // overflow: TextOverflow.ellipsis,
+                                  // softWrap: false,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Color(0xffFA3F70)),
+                      paddinghorizontal(size: 20),
+                    ],
+                  ),
+                  paddingvertical(size: 25),
+                ],
+              )));
+    } else if (this.menu == 'help') {
+      return SizedBox(
+          width: MediaQuery.of(context).size.width - 40,
+          child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.white),
+              child: Column(
+                children: [
+                  paddingvertical(size: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      paddinghorizontal(size: 20),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                //width: 100,
+                                width: MediaQuery.of(context).size.width -
+                                    120, //this is the total width of your screen
+                                child: Text(
+                                  menuTitle,
+                                  style: TextStyle(
+                                      fontSize: 16, fontFamily: 'Roboto'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      menuIcon == 'phone'
+                          ? Icon(Icons.phone, color: Color(0xffFA3F70))
+                          : Icon(Icons.phone, color: Color(0xffFA3F70)),
+                      paddinghorizontal(size: 20),
+                    ],
+                  ),
+                  paddingvertical(size: 25),
+                ],
+              )));
+    } else if (this.menu == 'edit-profile') {
+      return SizedBox(
+          width: MediaQuery.of(context).size.width - 40,
+          child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  border: Border.all(color: Color(0xffECECEC), width: 3)),
+              child: Column(
+                children: [
+                  paddingvertical(size: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      paddinghorizontal(size: 10),
+                      Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(13),
+                              color: Color(0xffFFEFF4),
+                            ),
+                            width: 44,
+                            height: 54,
+                            child: Icon(
+                              Icons.description,
+                              color: Color(0xffFA3F70),
+                              size: 35,
+                            ),
+                          ),
+                          paddinghorizontal(size: 20),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                //width: 100,
+                                width: MediaQuery.of(context).size.width -
+                                    160, //this is the total width of your screen
+                                child: Text(
+                                  menuTitle,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      fontFamily: 'Roboto'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      paddinghorizontal(size: 20),
+                    ],
+                  ),
+                  paddingvertical(size: 15),
+                ],
+              )));
+    } else {
+      return SizedBox(
+          width: MediaQuery.of(context).size.width - 40,
+          child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.white),
+              child: Column(
+                children: [
+                  paddingvertical(size: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      paddinghorizontal(size: 20),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                //width: 100,
+                                width: MediaQuery.of(context).size.width -
+                                    120, //this is the total width of your screen
+                                child: Text(
+                                  menuTitle,
+                                  style: TextStyle(
+                                      fontSize: 16, fontFamily: 'Roboto'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Color(0xffFA3F70)),
+                      paddinghorizontal(size: 20),
+                    ],
+                  ),
+                  paddingvertical(size: 25),
+                ],
+              )));
+    }
   }
 }
